cb9644e8cb2afba05a93f8fa331a23c4b2706b11
Ryan Gambrell <rgambrell@ggc.edu>
2022-04-10 00:54:27 -0400

    Client Update: Added Functionality for Creating/Updating Movies.

--- src/main/java/com/example/movieapi/Client.java
+++ src/main/java/com/example/movieapi/Client.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -41,11 +40,11 @@ public class Client {
                     break;
                 }
                 case 3: {
-                    System.out.println("\ncreateMovie() hasn't been implemented yet.\n");
+                    cl.createOrUpdateMovie("create");
                     break;
                 }
                 case 4: {
-                    System.out.println("\nupdateMovie() hasn't been implemented yet.\n");
+                    cl.createOrUpdateMovie("update");
                     break;
                 }
                 case 5: {
@@ -156,6 +155,126 @@ public class Client {
             System.out.println("-------------------------------");
 
             System.out.println(selectedMovie + "\n");
+
+        }catch(HttpClientErrorException restEx)
+        {
+            System.out.println(parseResponseMessage(restEx.getMessage()) + "\n");
+        }
+    }
+
+    public void createOrUpdateMovie(String desiredAction)
+    {
+        // Create new movie and add movie attributes
+        // using what user has entered
+        Movie newMovie = new Movie();
+
+        // Prompt for id
+        int id = 0;
+        if(desiredAction.equals("update"))
+        {
+            try
+            {
+                System.out.print("\nEnter ID of movie you would like to update: ");
+                id = input.nextInt();
+                input.nextLine();
+                newMovie.setId(id);
+            }catch(InputMismatchException ime)
+            {
+                System.out.println("\nError: Not a number. Exiting... \n");
+                input.next();
+                return;
+            }
+        }
+
+        // Prompt for title
+        String title = "";
+        System.out.print("\nEnter title: ");
+        title = input.nextLine();
+        if(!title.equals(""))
+        {
+            newMovie.setTitle(title);
+        }
+
+        // Prompt for Genre
+        String genre = "";
+        System.out.print("\nEnter Genre: ");
+        genre = input.nextLine();
+        if(!genre.equals(""))
+        {
+            newMovie.setGenre(genre);
+        }
+
+        // Prompt for Rate
+        double rate = 0.0;
+        try
+        {
+            System.out.print("\nEnter Rate (a number): ");
+            String rateUnchecked = input.nextLine();
+            if(!rateUnchecked.equals(""))
+            {
+                newMovie.setRate(Double.parseDouble(rateUnchecked));
+            }
+        }catch(NumberFormatException ime)
+        {
+            System.out.println("\nError: Not a number. Exiting... \n");
+            return;
+        }
+
+        // Prompt for Description
+        String description = "";
+        System.out.print("\nEnter Description: ");
+        description = input.nextLine();
+        if(!description.equals(""))
+        {
+            System.out.println("Description set");
+            newMovie.setDescription(description);
+        }
+
+        // Prompt for RateNum
+        double rateNum = 0;
+        try
+        {
+            System.out.print("\nEnter RateNum: ");
+            String rateNumUnchecked = input.nextLine();
+            if(!rateNumUnchecked.equals(""))
+            {
+                newMovie.setRateNum((int) Math.round(Double.parseDouble(rateNumUnchecked)));
+            }
+        }catch(NumberFormatException ime)
+        {
+            System.out.println("\nError: Not a number. Exiting... \n");
+            return;
+        }
+
+        RequestEntity<Movie> movieManipulationRequest = null;
+        if(desiredAction.equals("create"))
+        {
+            movieManipulationRequest = RequestEntity.post(baseURL + "/movie").body(newMovie);
+        }
+        if(desiredAction.equals("update"))
+        {
+            movieManipulationRequest = RequestEntity.put(baseURL + "/movies").body(newMovie);
+        }
+
+        try
+        {
+            ResponseEntity<Movie> response =
+                    restTemplate.exchange(movieManipulationRequest, Movie.class);
+
+            Movie resultMovie = response.getBody();
+
+            if(desiredAction.equals("create"))
+            {
+                System.out.println("\nCreated Movie");
+            }
+            if(desiredAction.equals("update"))
+            {
+                System.out.println("\nUpdated Movie");
+            }
+
+            System.out.println("-------------------------------");
+            System.out.println("-------------------------------");
+            System.out.println(resultMovie + "\n");
 
         }catch(HttpClientErrorException restEx)
         {
